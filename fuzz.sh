e7021f3a19d4a8bb700da3b0dece292b211aaf27
Caleb James DeLisle <cjd@cjdns.fr>
2018-10-12 18:26:58 +0200

    shell is hard, ok?

--- fuzz.sh
+++ fuzz.sh
@@ -3,7 +3,6 @@ die() { echo "Error $1"; exit 100; }
 
 OUTPUT_DIR="./findings";
 
-JOBS=
 [ "x" = "x$JOBS" ] && which nproc 2>/dev/null >/dev/null && JOBS=`nproc --all`;
 [ "x" = "x$JOBS" ] && which grep 2>/dev/null >/dev/null && [ -f /proc/cpuinfo ] && \
     JOBS=`grep -c ^processor /proc/cpuinfo`;
@@ -11,7 +10,7 @@ JOBS=
 
 i=0; while [ "x$i" != "x$JOBS" ]; do
     ## Safety
-    [ "x$i" == "x64" ] die "JOBS doesn't seem to be a number between 0 and 64";
+    [ "x$i" == "x64" ] && die "JOBS doesn't seem to be a number between 0 and 64";
     i=$(expr $i + 1);
 done
 
@@ -25,14 +24,18 @@ i=0; while [ "x$i" != "x$JOBS" ]; do
     echo "-------------------------------------------------------"
     echo "Launching cjdnsfuzz-$i";
     echo;
+    echo '' >./logs/cjdnsfuzz-$i.log
     ./afl-*/afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR $MS "cjdnsfuzz-$i" -- \
-        $CJDNS_DIR/test_testcjdroute_c fuzz --quiet --stderr-to errout.txt --inittests 2>&1 >./logs/cjdnsfuzz-$i.log &
+        $CJDNS_DIR/test_testcjdroute_c fuzz --quiet --stderr-to errout.txt --inittests \
+            2>>./logs/cjdnsfuzz-$i.log >>./logs/cjdnsfuzz-$i.log &
     AFL_PID=$!;
     tail -f ./logs/cjdnsfuzz-$i.log &
     TAIL_PID=$!;
+    # If we get ^C during the process, we still want to kill the tail process...
+    ( sleep 5 ; kill $TAIL_PID 2>/dev/null ) &
     sleep 5;
-    kill $TAIL_PID
-    kill -0 $AFL_PID || die "Failed to startup fuzz process, some processes may still be running";
+    kill $TAIL_PID 2>/dev/null
+    kill -0 $AFL_PID || break;
     echo;
     echo -e "Launched cjdnsfuzz-$i as pid $AFL_PID";
     echo;

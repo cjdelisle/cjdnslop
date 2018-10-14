c4973b83cc419e484b423df4c4ab884a8c3a60e8
Caleb James DeLisle <cjd@cjdns.fr>
2018-10-14 21:19:29 +0200

    Fixed bug in debug_crashes.sh

--- debug_crashes.sh
+++ debug_crashes.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 CJDNS_DIR=`echo ./cjdns/build_*`;
-ls ./findings/ | while read x; do
-    ls ./findings/$x/crashes | while read y; do
-        echo "Trying out: ./findings/$x/crashes/$y";
-        echo;
-        gdb $CJDNS_DIR/test_testcjdroute_c \
+for x in `ls ./findings/`; do
+    for y in `ls ./findings/$x/crashes`; do
+	echo "Trying out: ./findings/$x/crashes/$y";
+	gdb $CJDNS_DIR/test_testcjdroute_c \
             -ex 'set confirm on' \
             -ex "r fuzz --quiet --inittests < ./findings/$x/crashes/$y" \
             -ex 'quit';
